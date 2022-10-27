bcc65759ffa8891264ed8c08a1b6cec3393cac83
Jettison <ilmsmjettison@gmail.com>
2022-10-27 21:55:04 +0800

    Add footer

---
+++ agents/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "../style.css";
+
+function Footer() {
+    return (
+        <footer>
+            <div className="line__break"></div>
+            <span>
+            <a href="#"><i className="fa-brands fa-square-github"></i></a> <a href="#"><i className="fa-brands fa-linkedin"></i></a></span>
+            <p>Made with ❤ by <a href="https://github.com/Jettiso">Jettison</a></p>
+        </footer>
+    );
+}
+
+export default Footer;
