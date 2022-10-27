ab0ba59b5110a81b8c8e634b2b3b7ee7b59f84c8
Jettison <ilmsmjettison@gmail.com>
2022-10-27 15:12:47 +0800

    add Character Display

--- agents/src/components/Character.jsx
+++ agents/src/components/Character.jsx
@@ -1,38 +1,11 @@
-
-
-
-function Character({ info, setCharInfoHandler }) {
-
-    const result = info.map((char) => {
-        let { displayName, displayIconSmall, background, description, fullPortrait, uuid } = char;
-
-        return (
-            <div className="character" key={uuid} onClick={(e) => {
-                setCharInfoHandler({
-                    displayName: {displayName},
-                    displayIconSmall: {displayIconSmall}, 
-                    description: {description},
-                    background: {background},
-                    fullPortrait: {fullPortrait},
-                    uuid : {uuid}
-            
-                })
-            }}>
-                <h1>{displayName}</h1>
-                <img src={displayIconSmall} alt={displayName} />
-                <p className="description">{description}</p>
-                <img src={background} alt="background" />
-                <img src={fullPortrait} alt="portrait" />
-            </div>
-        )
-    }) 
+function Character({ characterInfo }) {
+    const { displayName, description, fullPortrait, uuid } = characterInfo;
 
 
     return (
-        <>
-        {result}
-        </>
+        <div className="character__container">
+            <img src={fullPortrait} alt="" />
+        </div>
     )
 }
-
 export default Character;
